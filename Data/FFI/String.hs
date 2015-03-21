832f9e9a55c874562131e438d9a6125b454a8c4a
Hiroki Hattori <seagull.kamome@gmail.com>
2015-03-22 00:51:19 +0900

    タグレスな実装にした

--- Data/FFI/String.hs
+++ Data/FFI/String.hs
@@ -8,9 +8,13 @@
 --
 module Data.FFI.String where
 
+import Control.Applicative (Applicative, pure, (<$>))
+import Control.Impossible
 import GHC.TypeLits
 import Data.Proxy
-
+import qualified Data.HashTable.ST.Basic as HT
+import Control.Monad.ST.Safe
+-- import Data.Maybe (maybe)
 
 newtype FFI (n :: Nat) t a = FFI {
   runFFI :: forall r. Monad t =>
@@ -18,22 +22,26 @@ newtype FFI (n :: Nat) t a = FFI {
             -> (t String -> r)
             -> r
   }
---            -> (forall b. FFI t (a -> b) -> t String -> r)-> r }
 
-ffiFun :: (KnownNat n, Monad t) => String -> Proxy n -> FFI n t a
-ffiFun fmt arity = FFI $ \f _ -> f fmt arity []
+ffi :: (KnownNat n, Applicative t) => String -> Proxy n -> FFI n t a
+ffi fmt arity = if natVal arity == 0
+                then FFI $ \_ f -> f (pure fmt)
+                else FFI $ \f _ -> f fmt arity []
+{-# INLINE ffi #-}
 
 ffiVal :: Monad t => t String -> FFI 0 t a
 ffiVal x = FFI $ \_ f -> f x
+{-# INLINE ffiVal #-}
 
 ffiApply :: (KnownNat n, Monad t) => FFI (n + 1) t (a -> b) -> FFI 0 t a -> FFI n t b
 ffiApply (FFI f) (FFI g) =
   FFI $ \h p ->
-         g (error "") $ \x ->
+         g impossible $ \x ->
                          f (\fmt aty args ->
                              if natVal aty == 1
                              then p $ renderFFI fmt (args ++ [x])
-                             else h fmt Proxy (args ++ [x]) ) (error "")
+                             else h fmt Proxy (args ++ [x]) ) impossible
+
 
 
 renderFFI :: Monad t => String -> [t String] -> t String
@@ -47,10 +55,21 @@ renderFFI fmt args = sequence args >>= return . go "" fmt where
   go xs (c:ys) zs = go (xs ++ [c]) ys zs
 
 
+----------------------------------------------------------------------------
+
+newtype Q a = Q ( forall s. HT.HashTable s String Int -> ST s a )
+
+runQ :: Q a -> ST s a
+runQ (Q f) = HT.new >>= f
+
+newName :: String -> Q String
+newName prefix = Q $ \ht -> do
+  n <- maybe 0 (+ 1) <$> HT.lookup ht prefix
+  HT.insert ht prefix n
+  return $ prefix ++ show n
 
 
 
 ----------------------------------------------------------------------------
-
 
 
