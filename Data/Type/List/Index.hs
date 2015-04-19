67bc955f7b8f46758b361ddcc8354cfdc1e96640
Hiroki Hattori <seagull.kamome@gmail.com>
2015-04-20 00:08:38 +0900

    fix some bugs

--- Data/Type/List/Index.hs
+++ Data/Type/List/Index.hs
@@ -11,6 +11,7 @@ import GHC.TypeLits
 import Data.Type.Equality ((:~:) (..), TestEquality (..))
 import Data.Proxy
 import Unsafe.Coerce (unsafeCoerce)
+import Algebra.Absurd
 
 import Data.Type.List
 
@@ -21,7 +22,7 @@ newtype Index l e = Index Integer
 instance TestEquality (Index l) where
   testEquality (Index i) (Index j)
     | i == j = Just (unsafeCoerce Refl)
-    | otherwise = error "non-trivial Index"
+    | otherwise = Nothing
 
 zero :: Index (e ': l) e
 zero = Index 0
@@ -29,10 +30,10 @@ zero = Index 0
 index :: forall e l. Member e l => Index l e
 index = Index $ natVal (Proxy :: Proxy (IndexOf e l))
 
-absurd :: Index '[] e -> a
-absurd (Index i) = i `seq` error "absurd Index"
+instance Absurd (Index '[] e) where
+ absurd (Index i) = i `seq` error "absurd Index"
 
-trivial :: Index [e] f -> f :~: e
+trivial :: Index '[e] f -> f :~: e
 trivial (Index i)
   | i == 0 = unsafeCoerce Refl
   | otherwise = error "non-trivial Index"
@@ -42,12 +43,12 @@ trivial (Index i)
 push :: Index l e -> Index (f ': l) e
 push (Index i) = Index $ i + 1
 
-pop :: Index (f ': l) e -> Index l e
+pop :: Index (f ': l) e -> Either (f :~: e) (Index l e)
 pop (Index i)
-  | i == 0 = error "empty index"
-  | otherwise = Index $ i - 1
+  | i == 0 = Left $ unsafeCoerce Refl
+  | otherwise = Right $ Index $ i - 1
 
-swap :: Index (e ': f ': l) e -> Index (f ': e ': l) e
+swap :: Index (e ': f ': l) a -> Index (f ': e ': l) a
 swap (Index i)
   | i == 0 = Index 1
   | i == 1 = Index 0
